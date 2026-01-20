69d84a04c7455fcd68a2e8783a61a4d04f46d73c
Jeff Barfield <jeffreybarfield@protonmail.com>
2026-01-19 20:02:05 -0500

    Add submodules in config/resources module

--- src/config/resources.rs
+++ src/config/resources.rs
@@ -1,75 +1,105 @@
-use bevy::prelude::*;
-use serde::{Deserialize, Serialize};
-use std::path::PathBuf;
+pub use base::*;
+pub use settings::*;
+pub use world::*;
 
-use crate::camera::ZoomSpeed;
+pub mod base {
+    use bevy::prelude::*;
+    use serde::{Deserialize, Serialize};
+    use std::path::PathBuf;
 
-#[derive(Resource, Clone, Default, Eq, PartialEq, Hash, Debug, Serialize, Deserialize, Reflect)]
-pub struct ConfigPath(pub PathBuf);
+    #[derive(
+        Resource, Clone, Default, Eq, PartialEq, Hash, Debug, Serialize, Deserialize, Reflect,
+    )]
+    pub struct ConfigPath(pub PathBuf);
 
-#[derive(Resource, Clone, Default, Eq, PartialEq, Hash, Debug, Serialize, Deserialize)]
-pub struct ActiveWorldPath(pub PathBuf);
+    #[derive(Resource, Clone, Default, Eq, PartialEq, Hash, Debug, Serialize, Deserialize)]
+    pub struct ActiveWorldPath(pub PathBuf);
 
-#[derive(Resource, Clone, Default, Eq, PartialEq, Hash, Debug, Serialize, Deserialize, Reflect)]
-pub struct ParticleTypesFile(pub PathBuf);
+    #[derive(
+        Resource, Clone, Default, Eq, PartialEq, Hash, Debug, Serialize, Deserialize, Reflect,
+    )]
+    pub struct ParticleTypesFile(pub PathBuf);
 
-#[derive(Resource, Clone, Eq, PartialEq, Hash, Debug, Serialize, Deserialize)]
-pub struct InitConfig {
-    settings_init_file: PathBuf,
-    particle_types_init_file: PathBuf,
-    active_world_path: PathBuf,
-}
+    #[derive(Resource, Clone, Eq, PartialEq, Hash, Debug, Serialize, Deserialize)]
+    pub struct InitConfig {
+        settings_init_file: PathBuf,
+        particle_types_init_file: PathBuf,
+        active_world_path: PathBuf,
+    }
 
-impl Default for InitConfig {
-    fn default() -> Self {
-        Self {
-            settings_init_file: PathBuf::from("settings.toml"),
-            particle_types_init_file: PathBuf::from("particles.scn.ron"),
-            active_world_path: PathBuf::from("default"),
+    impl Default for InitConfig {
+        fn default() -> Self {
+            Self {
+                settings_init_file: PathBuf::from("settings.toml"),
+                particle_types_init_file: PathBuf::from("particles.scn.ron"),
+                active_world_path: PathBuf::from("default"),
+            }
+        }
+    }
+
+    impl InitConfig {
+        pub fn active_world_path(&self) -> &PathBuf {
+            &self.active_world_path
         }
     }
 }
 
-impl InitConfig {
-    pub fn active_world_path(&self) -> &PathBuf {
-        &self.active_world_path
+pub mod world {
+    use bevy::prelude::*;
+    use serde::{Deserialize, Serialize};
+
+    use crate::camera::ZoomSpeed;
+
+    #[derive(Resource, Clone, Default, Debug, Serialize, Deserialize)]
+    pub struct WorldConfig {
+        pub camera: CameraConfig,
     }
-}
 
-#[derive(Resource, Clone, Default, Debug, Serialize, Deserialize)]
-pub struct WorldConfig {
-    pub camera: CameraConfig,
-}
+    #[derive(Resource, Clone, Debug, Serialize, Deserialize)]
+    pub struct CameraConfig {
+        pub scale: f32,
+        pub zoom_speed: ZoomSpeed,
+        pub position: Vec2,
+    }
 
-#[derive(Resource, Clone, Debug, Serialize, Deserialize)]
-pub struct CameraConfig {
-    pub scale: f32,
-    pub zoom_speed: ZoomSpeed,
-    pub position: Vec2,
-}
+    impl Default for CameraConfig {
+        fn default() -> Self {
+            let scale = 0.25;
+            Self {
+                scale,
+                zoom_speed: ZoomSpeed(8.0),
+                position: Vec2::ZERO,
+            }
+        }
+    }
 
-impl Default for CameraConfig {
-    fn default() -> Self {
-        let scale = 0.25;
-        Self {
-            scale,
-            zoom_speed: ZoomSpeed(8.0),
-            position: Vec2::ZERO,
+    impl CameraConfig {
+        pub fn from_query(query: &Single<(&Transform, &ZoomSpeed, &Projection)>) -> Self {
+            let scale = match query.2 {
+                Projection::Orthographic(ortho) => ortho.scale,
+                _ => unreachable!(),
+            };
+            let position = Vec2::new(query.0.translation.x, query.0.translation.y);
+            Self {
+                scale,
+                zoom_speed: query.1.clone(),
+                position,
+            }
         }
     }
 }
 
-impl CameraConfig {
-    pub fn from_query(query: &Single<(&Transform, &ZoomSpeed, &Projection)>) -> Self {
-        let scale = match query.2 {
-            Projection::Orthographic(ortho) => ortho.scale,
-            _ => unreachable!(),
-        };
-        let position = Vec2::new(query.0.translation.x, query.0.translation.y);
-        Self {
-            scale,
-            zoom_speed: query.1.clone(),
-            position,
-        }
+pub mod settings {
+    use bevy::prelude::*;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Resource, Clone, Default, Debug, Serialize, Deserialize)]
+    pub struct SettingsConfig {}
+
+    pub struct KeyBindings {
+        pub pan_camera_up: KeyCode,
+        pub pan_camera_right: KeyCode,
+        pub pan_camera_down: KeyCode,
+        pub pan_camera_left: KeyCode,
     }
 }
