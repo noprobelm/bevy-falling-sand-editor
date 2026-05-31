89eb2d80d29c1713df79318326c21004d9beddf4
Jeff Barfield <jeffreybarfield@protonmail.com>
2026-05-31 18:01:11 -0400

    Move cursor viz handling logic to the cursor module; fix logic to show the cursor when in SelectedTool::Select

--- src/ui/states.rs
+++ src/ui/states.rs
@@ -1,19 +1,12 @@
-use bevy::{
-    prelude::*,
-    window::{CursorOptions, PrimaryWindow},
-};
+use bevy::prelude::*;
 use bevy_egui::{EguiContexts, EguiPrimaryContextPass};
-
-use crate::tools::{PreviousSelectedTool, SelectedTool};
 
 pub struct UiStatePlugin;
 
 impl Plugin for UiStatePlugin {
     fn build(&self, app: &mut App) {
         app.init_state::<UiState>()
-            .add_systems(EguiPrimaryContextPass, handle_ui_state)
-            .add_systems(OnEnter(UiState::Menu), show_cursor)
-            .add_systems(OnEnter(UiState::Canvas), hide_cursor);
+            .add_systems(EguiPrimaryContextPass, handle_ui_state);
     }
 }
 
@@ -51,12 +44,4 @@ fn handle_ui_state(
     }
 
     Ok(())
-}
-
-fn show_cursor(mut cursor_options: Single<&mut CursorOptions, With<PrimaryWindow>>) {
-    cursor_options.visible = true;
-}
-
-fn hide_cursor(mut cursor_options: Single<&mut CursorOptions, With<PrimaryWindow>>) {
-    cursor_options.visible = false;
 }
